7c9955c13c515fa6eb53dd47c0b616a29d9357d3
sungm1n <w0451513@nscc.ca>
2021-03-22 01:34:20 +0900

    Finished all

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -16,15 +16,17 @@ public class MainFrame extends JFrame implements ActionListener {
     private JLabel lblQuestion;
     private JLabel lblHint;
     private JTextField [] nums = {new JTextField(), new JTextField(), new JTextField()};
-	private JTextField resultArea;
-	private TextFieldValidator[] validators = new TextFieldValidator[3];;
+	private JTextArea resultArea;
+	private TextFieldValidator[] validators = new TextFieldValidator[3];
 	private JButton btnOk;
 	private JButton btnReset;
-	private int [] fermi = new int[3];
+	private int [] fermi = new int[3]; // correct answer
 	private Random rand = new Random();
+	private int counts = 0;
 	
 	public MainFrame() {
 		
+		// randomly create the answer
 		for (int i = 0; i < 3; i++) {
 			int num = rand.nextInt(10);
 			for (int j = 0; j < i; j++) {
@@ -45,11 +47,12 @@ public class MainFrame extends JFrame implements ActionListener {
         getContentPane().setLayout(null);
 		
 		lblTitle = new JLabel("Fermi Guessing Game");
-		lblTitle.setFont(new Font("Gulim", Font.BOLD, 12));
+		lblTitle.setFont(new Font("Courier", Font.BOLD, 13));
 		lblTitle.setBounds(12, 21, 159, 15);
 		getContentPane().add(lblTitle);
 		
 		lblQuestion = new JLabel("Enter your three guesses (0-9):");
+		lblQuestion.setFont(new Font ("Courier", Font.PLAIN, 12));
 		lblQuestion.setBounds(12, 46, 184, 15);
 		getContentPane().add(lblQuestion);
 		
@@ -63,24 +66,28 @@ public class MainFrame extends JFrame implements ActionListener {
 		nums[2].setBounds(12, 133, 60, 21);
 		
 		btnOk = new JButton("Ok");
-		btnOk.setBounds(12, 164, 60, 37);
+		btnOk.setBounds(12, 164, 60, 32);
 		btnOk.addActionListener(this);
 		getContentPane().add(btnOk);
 		
 		btnReset = new JButton("Reset");
-		btnReset.setBounds(12, 318, 105, 42);
+		btnReset.setBounds(12, 318, 75, 42);
 		btnReset.addActionListener(this);
 		getContentPane().add(btnReset);
 		
-		lblHint = new JLabel("Hints");
+		lblHint = new JLabel("Hints:");
+		lblHint.setFont(new Font ("Courier", Font.PLAIN, 12));
 		lblHint.setBounds(231, 46, 57, 15);
 		getContentPane().add(lblHint);
 		
-		resultArea = new JTextField();
+		resultArea = new JTextArea();
 		resultArea.setBounds(231, 71, 219, 289);
-		resultArea.setEnabled(false);					// 맞는지 체크하기 ! ! !
+		resultArea.setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY, 1));
+		resultArea.setFont(new Font("Courier", Font.PLAIN, 12));
+		resultArea.setForeground(Color.BLACK);
+		resultArea.setEditable(false);		// 수정
+		resultArea.setColumns(15);
 		getContentPane().add(resultArea);
-		resultArea.setColumns(10);
 		
 		for (int i = 0; i < 3; i++) {
 			validators[i] =  new TextFieldValidator(nums[i]);
@@ -112,10 +119,11 @@ public class MainFrame extends JFrame implements ActionListener {
 					}
 				}
 				fermi[i] = num;
-			}			
+			}
+			btnOk.setEnabled(true);
 		}
-		
 		if (event.getSource() == btnOk) {
+			counts++;
 			boolean checkValidator = true;
 			for (int i = 0; i < 3; i++) {
 				if(!validators[i].check())
@@ -124,7 +132,40 @@ public class MainFrame extends JFrame implements ActionListener {
 			if (!(checkValidator)) {
 				return;
 			}
+			String guesses = "";
+			for (int i = 0;i < 3; i++) {
+				guesses += nums[i].getText() + " ";
+			}
+			guesses += ": ";
 			
+			String [] hint = new String[3];
+			for (int i = 0; i < 3; i++) {
+				boolean found = false;
+				int n = Integer.parseInt(nums[i].getText());
+				for (int j = 0; j < 3; j++) {
+					if (n == fermi[j]) {
+						found = true;
+						if (i == j) {
+							hint[i] = "Fermi";
+						}
+						else {
+							hint[i] = "Pico";
+						}
+					}
+				}
+				if(!found) hint[i] = "Nano";
+			}
+			boolean gameOver = true;
+			for(int i = 0; i < 3; i++) {
+				guesses += hint[i] + " ";
+				if (hint[i] != "Fermi") gameOver = false;
+			}
+			
+			resultArea.setText(resultArea.getText() + guesses+ "\n");
+		
+			if (gameOver) { 
+				resultArea.append("Congratulations! Guesses: " + counts); // 이 부분 하나 남음
+			}
 		}		
 	}
 	
